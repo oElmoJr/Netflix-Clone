c3b4879852dcdd60b3b243787508f9ff528640a9
oElmoJr <elmojuniorduarte@gmail.com>
2022-05-03 23:06:56 -0300

    feature: add information card of movie/tv

--- js/main.js
+++ js/main.js
@@ -33,12 +33,14 @@ function showTV(data) {
   carroselSeries.innerHTML = "";
 
   data.forEach((movie) => {
-    const { poster_path } = movie;
+    let desc = movie.overview;
+    newDesc = desc.replace(/"/g, "");
+
     const movieEl = document.createElement("div");
     movieEl.classList.add("item");
 
     movieEl.innerHTML = `
-    <img class="box-filme" src="${IMG_URL + poster_path}" alt="" />
+    <img class="box-filme" src="${IMG_URL + movie.poster_path}" alt="" onclick="maisInformacoes('${IMG_URL + movie.backdrop_path}', '${movie.name}', '${newDesc}', '${movie.vote_average}', '${movie.first_air_date}')" />
     `;
 
     carroselSeries.appendChild(movieEl);
@@ -55,15 +57,19 @@ function getMovies(url) {
 }
 
 function showMovies(data) {
+
+  
   carroselFilme.innerHTML = "";
 
   data.forEach((movie) => {
-    const { poster_path } = movie;
+    let desc = movie.overview;
+    newDesc = desc.replace(/"/g, "");
+
     const movieEl = document.createElement("div");
     movieEl.classList.add("item");
 
     movieEl.innerHTML = `
-    <img class="box-filme" src="${IMG_URL + poster_path}" alt="" />
+    <img class="box-filme" src="${IMG_URL + movie.poster_path}" alt="" onclick="maisInformacoes('${IMG_URL + movie.backdrop_path}', '${movie.title}', '${newDesc}', '${movie.vote_average}', '${movie.release_date}')" />
     `;
 
     carroselFilme.appendChild(movieEl);
@@ -71,10 +77,14 @@ function showMovies(data) {
 
   owl();
 
-  let desc = data[0].overview;
+ let indice = parseInt(Math.random() * 20) 
+console.log(indice)
+
+  let desc = data[indice].overview;
+  newDesc = desc.replace(/"/g, "");
 
   bannerContainer.innerHTML = `
-    <h3 class="titulo">${data[0].title}</h3>
+    <h3 class="titulo">${data[indice].title}</h3>
     <p class="descricao">
       ${desc.substring(0, 149)}...
     </p>
@@ -85,7 +95,11 @@ function showMovies(data) {
         </span>
         ASSISTIR AGORA
       </button>
-      <button role="button" class="botao">
+      <button role="button" class="botao" onclick="maisInformacoes('${
+        IMG_URL + data[indice].backdrop_path
+      }', '${data[indice].title}', '${newDesc}', '${data[indice].vote_average}', '${
+    data[indice].release_date
+  }')">
         <span class="icon">
           <i class="fa-solid fa-circle-info"></i>
         </span>
@@ -97,13 +111,73 @@ function showMovies(data) {
   document.getElementById(
     "banner"
   ).style.background = `linear-gradient(to right,rgba(0, 0, 0), rgba(0, 0, 0, 0.850), rgba(0, 0, 0, 0) 130%), url('${
-    IMG_URL + data[0].backdrop_path
+    IMG_URL + data[indice].backdrop_path
   }') `;
 
   document.getElementById("banner").style.backgroundSize = "contain";
   document.getElementById("banner").style.backgroundPosition = "right";
 
   owl();
+}
+
+function toggleScroll() {
+  document.body.classList.toggle("noScroll");
+}
+
+
+function fecharInfo() {
+  cardSection = document.getElementById("cardSection");
+  cardSection.innerHTML = ""
+  toggleScroll();
+}
+
+function maisInformacoes(cartaz, titulo, descricao, nota, data) {
+  
+  cardSection = document.getElementById("cardSection");
+
+  window.scrollTo(0, 0);
+
+  toggleScroll();
+
+  cardSection.innerHTML = `
+  <div id="cardContainer" class="cardContainer" >
+  <button class="botao-close" onclick="fecharInfo()">
+    <span class="icon">
+    <i class="fa-solid fa-xmark"></i>
+    </span>
+  </button>
+  <div class="card" id="card" >
+    <img class="cartaz" src="${cartaz}" alt="">
+    <div class="cardInfos">
+    
+      <h2 class="titulo">${titulo}</h2>
+      <div>
+        <button class="botao">
+          <span class="icon">
+            <i class="fa-solid fa-play"></i>
+          </span>
+          Assistir</button>
+
+        <button class="botao-circle">
+          <span class="icon">
+            <i class="fa-solid fa-plus"></i>
+          </span>
+          </button>
+        <button class="botao-circle">
+          <span class="icon">
+            <i class="fa-solid fa-thumbs-up"></i>
+          </span>
+          </button>
+      </div>
+      <p class="infos"><span class="nota">Nota: ${nota}</span>
+       ${data.substring(0, 4)}
+       </p>
+      <p class="descricao">${descricao}</p>
+    
+    </div>
+  </div>
+</div>
+  `;
 }
 
 function owl() {
